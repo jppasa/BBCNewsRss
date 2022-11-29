c9e57a9756b288a7029e8b842c99dcf8bab77ec1
Juan Pablo Villegas <jppasa@gmail.com>
2022-11-28 19:12:29 -0600

    run network requests in IO thread

--- app/src/main/java/dev/jpvillegas/bbcnewsrss/data/repository/RssFeedRepositoryImpl.kt
+++ app/src/main/java/dev/jpvillegas/bbcnewsrss/data/repository/RssFeedRepositoryImpl.kt
@@ -1,10 +1,13 @@
 package dev.jpvillegas.bbcnewsrss.data.repository
 
+import android.util.Log
 import com.prof.rssparser.Parser
 import dev.jpvillegas.bbcnewsrss.common.Constants
 import dev.jpvillegas.bbcnewsrss.data.network.model.toRssFeed
 import dev.jpvillegas.bbcnewsrss.domain.model.RssFeed
 import dev.jpvillegas.bbcnewsrss.domain.repository.RssFeedRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import javax.inject.Inject
@@ -15,23 +18,26 @@ class RssFeedRepositoryImpl @Inject constructor(
 ) : RssFeedRepository {
 
     override suspend fun getRssFeeds(): List<RssFeed> {
-        return Constants.rssFeedUrls.mapNotNull { feedUrl ->
-            // Future: Check built-in cache in parser -> parser.getChannel(feedUrl)
+        return withContext(Dispatchers.IO) {
+            Constants.rssFeedUrls.mapNotNull { feedUrl ->
+                // Future: Check built-in cache in parser -> parser.getChannel(feedUrl)
+                val request = Request.Builder()
+                    .url(feedUrl)
+                    .build()
 
-            val request = Request.Builder()
-                .url(feedUrl)
-                .build()
-
-            val response = client
-                .newCall(request)
-                .execute()
-
-            val body = response.body?.string()
-
-            if (body == null) null
-            else feedUrl to body
-        }.map { (url, feedStr) ->
-            parser.parse(feedStr).toRssFeed(url)
+                try {
+                    client.newCall(request)
+                        .execute()
+                        .body?.string()
+                } catch (e: Exception) {
+                    null
+                }?.let {
+                    feedUrl to it
+                }
+            }.map { (url, feedStr) ->
+                Log.d("FetchRssFeeds", "Parsing $url -> ${feedStr.take(10)}")
+                parser.parse(feedStr).toRssFeed(url)
+            }
         }
     }
 }
