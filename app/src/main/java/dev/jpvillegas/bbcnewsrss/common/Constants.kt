4000bf88ab1da347e2ac58c22619e6cf299d31db
Juan Pablo Villegas <jppasa@gmail.com>
2022-11-28 18:57:34 -0600

    Add icon. Fix urls

--- app/src/main/java/dev/jpvillegas/bbcnewsrss/common/Constants.kt
+++ app/src/main/java/dev/jpvillegas/bbcnewsrss/common/Constants.kt
@@ -2,15 +2,15 @@ package dev.jpvillegas.bbcnewsrss.common
 
 object Constants {
     val rssFeedUrls = listOf(
-        "http://feeds.bbci.co.uk/news/rss.xml",
-        "http://feeds.bbci.co.uk/news/world/rss.xml",
-        "http://feeds.bbci.co.uk/news/uk/rss.xml",
-        "http://feeds.bbci.co.uk/news/business/rss.xml",
-        "http://feeds.bbci.co.uk/news/politics/rss.xml",
-        "http://feeds.bbci.co.uk/news/health/rss.xml",
-        "http://feeds.bbci.co.uk/news/education/rss.xml",
-        "http://feeds.bbci.co.uk/news/science_and_environment/rss.xml",
-        "http://feeds.bbci.co.uk/news/technology/rss.xml",
-        "http://feeds.bbci.co.uk/news/entertainment_and_arts/rss.xml",
+        "https://feeds.bbci.co.uk/news/rss.xml",
+        "https://feeds.bbci.co.uk/news/world/rss.xml",
+        "https://feeds.bbci.co.uk/news/uk/rss.xml",
+        "https://feeds.bbci.co.uk/news/business/rss.xml",
+        "https://feeds.bbci.co.uk/news/politics/rss.xml",
+        "https://feeds.bbci.co.uk/news/health/rss.xml",
+        "https://feeds.bbci.co.uk/news/education/rss.xml",
+        "https://feeds.bbci.co.uk/news/science_and_environment/rss.xml",
+        "https://feeds.bbci.co.uk/news/technology/rss.xml",
+        "https://feeds.bbci.co.uk/news/entertainment_and_arts/rss.xml",
     )
 }
